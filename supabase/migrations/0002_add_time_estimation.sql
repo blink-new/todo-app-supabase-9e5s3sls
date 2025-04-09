fadcafce7143c24abfaabb941160cf78a2e5120c
ShadowWalker2014 <kai.jiabo.feng@gmail.com>
2025-04-09 02:35:44 +0000

    Fix Time Estimation Migration

--- supabase/migrations/0002_add_time_estimation.sql
+++ supabase/migrations/0002_add_time_estimation.sql
@@ -3,6 +3,9 @@
 alter table todos 
   add column time_estimate text;
 
+-- Drop existing index if it exists
+drop index if exists todos_time_estimate_idx;
+
 -- Create index for time estimates
 create index todos_time_estimate_idx on todos(time_estimate);
 
